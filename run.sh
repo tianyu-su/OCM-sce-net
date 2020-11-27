a024023be6cead3c057c1b7541ca3f978d37a758
tianyu <1377590675@qq.com>
2020-11-27 20:29:56 +0800

    up_sh

--- run.sh
+++ run.sh
@@ -1,5 +1,5 @@
 # https://github.com/mvasil/fashion-compatibility/blob/master/main.py
-python main_poluvore.py --datadir /media/tianyu/Software/workspaces/datasets/Fashion/polyvore \
+python main_poluvore.py --datadir ${POLY} \
  --polyvore_split nondisjoint --name polyvore_nondisjoint \
  --learned --epochs 100 --num_concepts 5  --embed_loss 5e-4  --mask_loss 5e-4 \
- --batch-size 64 --num-workers 8
+ --batch-size 78 --num-workers 8
