d568dee12b61e3a8e2779cee62c72d1608cdcf08
tianyu <1377590675@qq.com>
2021-01-05 20:00:31 +0800

    up_sh

--- run.sh
+++ run.sh
@@ -1,5 +1,9 @@
 # https://github.com/mvasil/fashion-compatibility/blob/master/main.py
-python -u main_poluvore.py --datadir ${POLY} \
- --polyvore_split nondisjoint --name polyvore_nondisjoint \
- --learned --epochs 50 --num_concepts 5  --embed_loss 5e-4  --mask_loss 5e-4 \
- --batch-size 78 --num-workers 8
+CUDA_VISIBLE_DEVICES=1 python -u main_polyvore.py --datadir ./data \
+ --polyvore_split nondisjoint --name novse_nondisjoint \
+ --learned --epochs 15 --num_concepts 5  --embed_loss 5e-4  --mask_loss 5e-4 \
+ --batch-size 96 --num-workers 4  | tee logs/novse_nondisjoint.log;\
+CUDA_VISIBLE_DEVICES=1 python -u main_polyvore.py --datadir ./data \
+ --polyvore_split disjoint --name novse_disjoint \
+ --learned --epochs 15 --num_concepts 5  --embed_loss 5e-4  --mask_loss 5e-4 \
+ --batch-size 96 --num-workers 4  | tee logs/novse_disjoint.log
